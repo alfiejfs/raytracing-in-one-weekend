ec6611f66d0488f43f08bd55608fc146128419c3
alfiejfs <alfie@alfiesmith.net>
2023-04-18 11:38:07 +0100

    Alternate diffuse formulation

--- vec3.h
+++ vec3.h
@@ -123,4 +123,13 @@ vec3 random_unit_vector() {
     return unit_vector(random_in_unit_sphere());
 }
 
+vec3 random_in_hemisphere(const vec3& normal) {
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere, normal) > 0.0) {
+        return in_unit_sphere;
+    } else {
+        return -in_unit_sphere;
+    }
+}
+
 #endif
